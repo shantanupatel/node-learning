6a2d60ddf1754bafe00aebf0277133847f20ba6f
Shantanu Patel <shantu.patel@gmail.com>
2023-03-07 09:48:30 +0530

    deleted package-lock file and updated myApp.js with missing variable for mongoose

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ let bodyParser = require('body-parser');
 // var env = require('dotenv');
 require('dotenv').config();
 
+let mongoose;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('', function(req, res, next){
