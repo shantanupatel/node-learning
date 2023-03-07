1794c35770f091702e9a949e85e7fecd828e328b
Shantanu Patel <shantu.patel@gmail.com>
2023-03-07 09:19:48 +0530

    intial commit for starting with mongoose and mongodb

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 // var env = require('dotenv');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('', function(req, res, next){
   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
