c04b19de4f1ef7366153e8cb7cba90f74270789a
Shantanu Patel <shantu.patel@gmail.com>
2023-03-07 09:52:14 +0530

    fix for build failure

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ let bodyParser = require('body-parser');
 require('dotenv').config();
 
 let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
